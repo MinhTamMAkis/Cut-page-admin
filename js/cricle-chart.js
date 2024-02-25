3eb5498480970911d8b9a7660a6a63d6e81ae405
NGUYEN MINH TAM <94995197+MinhTamMAkis@users.noreply.github.com>
2024-02-26 01:23:23 +0700

    chart and todo

--- js/cricle-chart.js
+++ js/cricle-chart.js
@@ -5,36 +5,7 @@ const barColors = [
   "#0D99FF",
   "#000000",
 ];
-Chart.register({
-  id: 'titleInsidePlugin',
-  beforeDraw: chart => {
-    const { ctx, chartArea } = chart;
-    const { top, left, width, height } = chartArea;
 
-    // Draw title text
-    ctx.save();
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    const titleLine1 = '62';
-    const titleLine2 = 'Compete';
-    ctx.fillStyle = '#828690';
-    ctx.strokeStyle = '#FFFFFF';
-
-    ctx.font = '22px Arial';
-    ctx.lineWidth = 2; // Border width
-    ctx.strokeText(titleLine1, left + width / 2, top + height / 2 - 10);
-    ctx.fillText(titleLine1, left + width / 2, top + height / 2 - 10);
-
-    ctx.font = '11px Arial';
-    ctx.lineWidth = 1; 
-    
-    ctx.fillText(titleLine2, left + width / 2, top + height / 2 + 10);
-    ctx.strokeText(titleLine2, left + width / 2, top + height / 2 + 10);
-
-    
-    ctx.restore();
-  }
-});
 new Chart("Chart_cricle", {
   type: "doughnut",
   data: {
@@ -46,7 +17,6 @@ new Chart("Chart_cricle", {
   },
   options: {
     plugins: {
-      titleInsidePlugin: {} ,
       legend: {
         display: true,
         position: 'right',
